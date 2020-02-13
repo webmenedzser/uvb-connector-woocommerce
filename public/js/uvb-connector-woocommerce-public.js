d6b3f43076d6e72917125a76b258bf5b1ec1b65c
Ottó Radics <otto@webmenedzser.hu>
2020-02-13 11:51:20 +0100

    Fixed an error which prevented JS bundle to connect to service if the user was already logged in.

--- public/js/uvb-connector-woocommerce-public.js
+++ public/js/uvb-connector-woocommerce-public.js
@@ -1,18 +1,33 @@
 (function( $ ) {
-	'use strict';
+    'use strict';
 
-	jQuery(document).ready(function($) {
-		$('input[name=billing_email]').change(function() {
-			var data = {
-				'action': 'check_if_email_is_flagged',
-				'email': $(this).val()
-			};
+    function checkUVBService(fieldValue) {
+        console.log('Connecting to UVB Service.');
 
-			// We can also pass the url value separately from ajaxurl for front end AJAX implementations
-			jQuery.post(ajax_object.ajax_url, data, function(response) {
-				$('body').trigger('update_checkout');
-			});
-		});
-	});
+        var data = {
+            'action': 'check_if_email_is_flagged',
+            'email': fieldValue
+        };
+
+        // We can also pass the url value separately from ajaxurl for front end AJAX implementations
+        jQuery.post(ajax_object.ajax_url, data, function(response) {
+            $('body').trigger('update_checkout');
+        });
+    }
+
+    jQuery(document).ready(function($) {
+        var billingEmailField = document.querySelector('input[name=billing_email]');
+        if (billingEmailField) {
+            var fieldValue = billingEmailField.value;
+
+            if (fieldValue) {
+                checkUVBService(fieldValue);
+            }
+        }
+
+        $('input[name=billing_email]').change(function() {
+            checkUVBService($(this).val());
+        });
+    });
 
 })( jQuery );
