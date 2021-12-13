434eb72b22eea449e6587490ad33adc4fa4e6c3f
Ottó Radics <otto@webmenedzser.hu>
2021-12-13 17:06:53 +0100

    Added flag column to Shop index page

--- admin/class-uvb-connector-woocommerce-admin.php
+++ admin/class-uvb-connector-woocommerce-admin.php
@@ -93,6 +93,12 @@ class UVBConnectorWooCommerce_Admin {
         return $order_statuses;
     }
 
+    public function addColumnFlagged($columns) {
+        $columns['uvb_status'] = 'Utánvét Ellenőr státusz';
+
+        return $columns;
+    }
+
     public function flagOrder($orderId)
     {
         $options = get_option('uvb_connector_woocommerce_options');
@@ -194,6 +200,20 @@ class UVBConnectorWooCommerce_Admin {
         $message = __($message, 'uvb-connector-woocommerce' );
 
         printf( '<div class="notice %1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
+    }
+
+    public function showFlagNoticeInColumn( $column ) {
+        global $post;
+
+        $flag = get_post_meta($post->ID, '_uvb_connector_woocommerce_flag', true);
+
+        if ('uvb_status' === $column) {
+            if ($flag) {
+                echo '<div style="display: flex; align-items: center; font-weight: bold; color: #db3535;"><svg xmlns="http://www.w3.org/2000/svg" style="width: 1.5rem; height: 1.5rem; margin-right: 1rem;" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd" /></svg> Figyelem!</div>';
+            } else {
+                echo '<div style="display: flex; align-items: center; font-weight: bold; color: #c6e1c6;"><svg xmlns="http://www.w3.org/2000/svg" style="width: 1.5rem; height: 1.5rem; margin-right: 1rem;" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg> Rendben.</div>';
+            }
+        }
     }
 
     /**
