1b0592c3d08ee591fa0a56f79a9f6c12ed1ef316
Ottó Radics <otto@webmenedzser.hu>
2022-10-10 11:45:19 +0200

    Version bump

--- uvb-connector-woocommerce.php
+++ uvb-connector-woocommerce.php
@@ -16,7 +16,7 @@
  * Plugin Name:       UV-B Connector for WooCommerce
  * Plugin URI:        https://utanvet-ellenor.hu
  * Description:       🚨 Kiszállításokat szűrünk és védünk.
- * Version:           1.7.4
+ * Version:           1.7.5
  * Author:            Utánvét Ellenőr
  * Author URI:        https://utanvet-ellenor.hu
  * License:           GPL-3.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'UVB_CONNECTOR_WOOCOMMERCE_VERSION', '1.7.4' );
+define( 'UVB_CONNECTOR_WOOCOMMERCE_VERSION', '1.7.5' );
 
 /**
  * Define vendor/autoload.php path used across the plugin
