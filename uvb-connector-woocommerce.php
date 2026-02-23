b1298c758cb8195c362be7984da9cba42b0efd40
Ottó Radics <iam@ottoradics.com>
2026-02-23 16:24:32 +0100

    Released 4.0.0

--- uvb-connector-woocommerce.php
+++ uvb-connector-woocommerce.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Utánvét Ellenőr
  * Plugin URI:        https://utanvet-ellenor.hu
  * Description:       🚨 Kiszállításokat szűrünk és védünk.
- * Version:           3.2.0
+ * Version:           4.0.0
  * Author:            Utánvét Ellenőr
  * Author URI:        https://utanvet-ellenor.hu
  * License:           GPL-3.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'UVB_CONNECTOR_WOOCOMMERCE_VERSION', '3.2.0' );
+define( 'UVB_CONNECTOR_WOOCOMMERCE_VERSION', '4.0.0' );
 
 /**
  * Define vendor/autoload.php path used across the plugin
